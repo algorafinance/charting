bb2d6bc7102913b21c562238aef744fb8f200273
kloomis1 <kloomis@algorafinance.com>
2017-03-05 21:24:09 -0600

    Cleaned up file and added comments.

--- ChartsQuantmod.R
+++ ChartsQuantmod.R
@@ -4,113 +4,36 @@
 # Charting examples on quantmod: http://www.quantmod.com/examples/charting/
 # Imports quantmod library to retrieve stock data from Yahoo! Finance
 library(quantmod)
-library(Quandl)
-# creates a list of stocks - future revisions will 
-# include importing tickers from .csv file
-
-# Read CSV into R. Check current working directory
-# getwd() and setwd() can be used to change directory
-AeroCSV <- read.csv(file="Tickers/Aerospace1.csv", header=TRUE, sep=",")
-
-for (x in AeroCSV[1]) {
-  print(as.character(AeroCSV[x,1]))
-}
-
-csvTickers <- c(as.character(AeroCSV[,1]))
-
-# symbolList <- c('NVDA','AMD','MU','INTC','WDC','AMAT','MRVL')
-symbolList <- c(as.character(AeroCSV[,1]))
- 
 require(quantmod)
-
-#Initialize a list to store the fetched prices
-dataList <- list()
-
-#Loop through symbols, fetch prices, and store in myList
-dataList <-lapply(symbolList, function(x) {getSymbols(x,auto.assign=FALSE)} )
-names(dataList) <- symbolList
 
 # Universal date
 timeframe <- '2017-01-01::2017-03-03'
 
-# Fetches and stores data from Yahoo! Finance
-getSymbols(symbolList)
+# creates a list of stocks - future revisions will 
+# include importing tickers from .csv file
+# Read CSV into R. Check current working directory
+# getwd() and setwd() can be used to change directory
+stocksCSV <- read.csv(file="Tickers/Aerospace1.csv", header=TRUE, sep=",")
 
+# symbolList <- c('NVDA','AMD','MU','INTC','WDC','AMAT','MRVL')
+# symboList includes ticker strings from 1st col from .csv file
+# converts stockCSV[,1] to strings
+symbolList <- c(as.character(stocksCSV[,1]))
+
+#Initialize a list to store the fetched prices
+dataList <- list()
+#Loop through symbols, fetch prices, and store in dataList
+dataList <-lapply(symbolList, function(x) {getSymbols(x,auto.assign=FALSE)} )
+names(dataList) <- symbolList
+
+# Fetches and stores data from Yahoo! Finance
+# Pauses data retrieval to avoid Yahoo Finance shutting system down
+getSymbols(symbolList)
+# Sys.sleep(10)
+
+# Iterates through symbolList and creates, names, and organizes charts
 for (i in symbolList) {
   pdf(file = paste0("/Users/kloomis/Desktop/Code/charting/RCharts/",i," - ",timeframe,".pdf"))
   candleChart(dataList[[i]], type = 'candles', name = paste0('Algora Finance - ',i), TA = 'addVo();addWMA()', theme = chartTheme('white'), up.col = 'green', dn.col = 'red', subset = timeframe, major.ticks = '1 hour', show.grid = TRUE)
   dev.off()
 }
-
-
-
-
-# ChartsQuantmod.R, Kyle Loomis, 2017-03-05
-
-# ==================================================================
-# Charting examples on quantmod: http://www.quantmod.com/examples/charting/
-# Imports quantmod library to retrieve stock data from Yahoo! Finance
-library(quantmod)
-library(Quandl)
-# creates a list of stocks - future revisions will 
-# include importing tickers from .csv file
-
-# Read CSV into R. Check current working directory
-# getwd() and setwd() can be used to change directory
-AeroCSV <- read.csv(file="Tickers/Finance.csv", header=TRUE, sep=",")
-
-for (x in 1:50) {
-  print(as.character(AeroCSV[x,1]))
-}
-
-csvTickers <- c(as.character(AeroCSV[,1]))
-
-# symbolList <- c('NVDA','AMD','MU','INTC','WDC','AMAT','MRVL')
-symbolList <- c(as.character(AeroCSV[,1]))
-
-require(quantmod)
-
-#Initialize a list to store the fetched prices
-dataList <- list()
-
-#Loop through symbols, fetch prices, and store in myList
-dataList <-lapply(symbolList, function(x) {getSymbols(x,auto.assign=FALSE)} )
-names(dataList) <- symbolList
-
-# Universal date
-timeframe <- '2017-01-01::2017-03-03'
-
-# Fetches and stores data from Yahoo! Finance
-getSymbols(symbolList)
-
-for (i in symbolList) {
-  pdf(file = paste0("/Users/kloomis/Desktop/Code/charting/RCharts/",i," - ",timeframe,".pdf"))
-  candleChart(dataList[[i]], type = 'candles', name = paste0('Algora Finance - ',i), TA = 'addVo();addWMA()', theme = chartTheme('white'), up.col = 'green', dn.col = 'red', subset = timeframe, major.ticks = '1 hour', show.grid = TRUE)
-  dev.off()
-}
-
-
-
-
-
-
-# Creates candlestick stock chart
-candleChart(NVDA, type = 'candles', name = 'Algora Finance - NVDA', TA = 'addVo()', theme = chartTheme('white'), up.col = 'green', dn.col = 'red', subset = '2017--01::2017-03-03', major.ticks = '1 hour', show.grid = TRUE)
-# Alternatively: candleChart(AMD, ...,  TA = 'addVo();addBBands()', ...)
-# reChart() used to modify original chart
-addBBands(n = 5, sd = 2, ma = "SMA", draw = 'bands', on = -1)
-addMACD()
-addSMA()
-
-
-
-
-# Creates candlestick stock chart
-candleChart(NVDA, type = 'candles', name = 'Algora Finance - NVDA', TA = 'addVo()', theme = chartTheme('white'), up.col = 'green', dn.col = 'red', subset = '2017--01::2017-03-03', major.ticks = '1 hour', show.grid = TRUE)
-# Alternatively: candleChart(AMD, ...,  TA = 'addVo();addBBands()', ...)
-# reChart() used to modify original chart
-addBBands(n = 5, sd = 2, ma = "SMA", draw = 'bands', on = -1)
-addMACD()
-addSMA()
-
-
--- README.md
+++ README.md
@@ -1,49 +1,49 @@
 # charting repository - created by Kyle Loomis on 2017-03-05
 
 
-# ==================================================================
-# ====================== Basic syntax of R =========================
+==================================================================
+====================== Basic syntax of R =========================
 
-# === Package Control ===
-# Install packages: install.packages("~library_name~")
-# Install packages from github - required only once
-#   1. library(devtools)
-#   2. install_github("twitter/AnomalyDetection")   # Requires repository name
-# Alternatively: 
-#   1. library(githubinstall)
-#   2. githubinstall("AnomalyDetection")
-# Import library - required before every used of the package
-# library(~libary_name~)
-# Syntax description: ?~function/library_name~
+=== Package Control ===
+Install packages: install.packages("~library_name~")
+Install packages from github - required only once
+  1. library(devtools)
+  2. install_github("twitter/AnomalyDetection")   # Requires repository name
+Alternatively: 
+  1. library(githubinstall)
+  2. githubinstall("AnomalyDetection")
+Import library - required before every used of the package
+library(~libary_name~)
+Syntax description: ?~function/library_name~
 
-# === syntax explanation ===
-# Assigning a variable: x <- 6
-# Combine values into vector/list: x <- c(values)
-# Statistical functions: mean(x), sd(x), sum(x)
-# Compare values: >, <, ==, >=, <=, !=
-# Indexing: x[1] for 1st value, x[2] for 2nd value
-# Matrix: matrix(c(values), byrow = TRUE, nrow = 3)
-#   1. rownames(my_matrix) <- row_names_vector
-#   2. colnames(my_matrix) <- col_names_vector
-# Summing: rowSums(my_matrix), colSums(my_matrix)
-# Combining matrices: 
-#   1. Bind new column: big_matrix <- cbind(matrix1, matrix2, vector1 ...)
-#   2. Bind new row: big_matrix <- rbind(matrix1, matrix2, vector1 ...)
+=== syntax explanation ===
+Assigning a variable: x <- 6
+Combine values into vector/list: x <- c(values)
+Statistical functions: mean(x), sd(x), sum(x)
+Compare values: >, <, ==, >=, <=, !=
+Indexing: x[1] for 1st value, x[2] for 2nd value
+Matrix: matrix(c(values), byrow = TRUE, nrow = 3)
+  1. rownames(my_matrix) <- row_names_vector
+  2. colnames(my_matrix) <- col_names_vector
+Summing: rowSums(my_matrix), colSums(my_matrix)
+Combining matrices: 
+  1. Bind new column: big_matrix <- cbind(matrix1, matrix2, vector1 ...)
+  2. Bind new row: big_matrix <- rbind(matrix1, matrix2, vector1 ...)
 
-# ==================================================================
-# Charting examples on quantmod: http://www.quantmod.com/examples/charting/
-# Imports quantmod library to retrieve stock data from Yahoo! Finance
+==================================================================
+Charting examples on quantmod: http://www.quantmod.com/examples/charting/
+Imports quantmod library to retrieve stock data from Yahoo! Finance
 
-# === Example of usage ===
+=== Example of usage ===
 library(quantmod)
-# Fetches data for the following symbols:
+Fetches data for the following symbols:
 getSymbols(c('NVDA','AMD'))
 
 chartSeries(NVDA)
-# Creates candlestick stock chart
+Creates candlestick stock chart
 candleChart(NVDA, type = 'candles', name = 'Algora Finance - NVDA', TA = 'addVo()', theme = chartTheme('white'), up.col = 'green', dn.col = 'red', subset = '2017--01::2017-03-03', major.ticks = '1 hour', show.grid = TRUE)
-# Alternatively: candleChart(AMD, ...,  TA = 'addVo();addBBands()', ...)
-# reChart() used to modify original chart
+Alternatively: candleChart(AMD, ...,  TA = 'addVo();addBBands()', ...)
+reChart() used to modify original chart
 addBBands(n = 5, sd = 2, ma = "SMA", draw = 'bands', on = -1)
 addMACD()
 addSMA()
